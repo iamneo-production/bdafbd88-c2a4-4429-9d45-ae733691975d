67ca4d5a5b751c6cb0a2208bba83b65937496206
SrivalliSuma <srivalli.suma10@gmail.com>
2023-07-22 13:17:35 +0000

    dv

--- dotnetapp/Webapi/Controllers/SignupController.cs
+++ dotnetapp/Webapi/Controllers/SignupController.cs
@@ -14,6 +14,19 @@ namespace Webapi.Controllers
             return Created("",dal.SaveUser(user));
         }
 <<<<<<< HEAD
+    }
+}
+=======
+<<<<<<< HEAD
+    }
+}
+=======
+<<<<<<< HEAD
+    }
+}
+
+=======
+<<<<<<< HEAD
 =======
 
         BusinessLayer dal = new BusinessLayer();
@@ -56,3 +69,6 @@ namespace Webapi.Controllers
 >>>>>>> 68c33ef6a8d67ca4dd708c1bbe958082c80ab738
     }
 }
+>>>>>>> 42a1df4aa7e60bdeac85759d215b4ca7fd16040d
+>>>>>>> 7159bf44cc39b8d8af92fc9321edd24026ad0a45
+>>>>>>> 46f7519582fbe2fdbe5b902d3bb9d8dfef0b9f34
