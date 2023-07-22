cc3848fd52b9a6169940e6fe07339127ead1b710
pavan1806 <pa1chintakayala@gmail.com>
2023-07-22 15:11:05 +0000

    mksd

--- reactapp/src/components/Auth/LoginAuth.jsx
+++ reactapp/src/components/Auth/LoginAuth.jsx
@@ -10,14 +10,7 @@ function LoginAuth(values){
         error.email=""
     }
 
-    if(values.password === ""){
-        error.password = "Password should not be empty"
-    }
-    else if(!password_pattern.test(values.password)) {
-        error.password = "Password should be 2 letters long."
-    } else {
-        error.password=""
-    }
+    
 
     return error;
 }
--- reactapp/src/components/Auth/SignupAuth.jsx
+++ reactapp/src/components/Auth/SignupAuth.jsx
@@ -1,26 +1,13 @@
 function SignupAuth(values) {
     let error = {}
-    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    
     const password_pattern = /^[a-zA-Z0-9]{8,}$/
     const username_pattern = /^[a-zA-Z0-9]{3,}$/ //alpha numeric character
     const mobilenumber_pattern = /^[0-9]{10}$/
   
-    if (!values.email) {
-      error.email = "Email should not be empty";
-    } else if (!email_pattern.test(values.email)) {
-      error.email = "Invalid email format";
-    } else {
-        error.email= "";
-    }
+
   
-    if (!values.password) {
-      error.password = "Password should not be empty";
-    } else if (!password_pattern.test(values.password)) {
-      error.password =
-        "Password must be at least 8 characters long";
-    } else {
-        error.password="";
-    }
+    
   
     if (!values.confirmPassword) {
       error.confirmPassword = "Confirm Password should not be empty";
