efc3c3bab2f4f24f79222675f091d608f299fc41
vineelanarra <narravineela0@gmail.com>
2023-07-23 12:58:50 +0000

    Overall code

---
+++ reactapp/src/components/Auth/LoginAuth.jsx
@@ -0,0 +1,18 @@
+function LoginAuth(values){
+    let error = {}
+    const password_pattern = /^[a-zA-Z0-9]{2,}$/
+    //const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+
+    if(values.email === "") {
+        error.email="Email should not be empty"
+    }
+     else {
+        error.email=""
+    }
+
+    
+
+    return error;
+}
+
+export default LoginAuth;
