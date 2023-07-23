3e42f5cdeb55ed5bb7fc5f042c1d61afa0e9593f
Karan71250 <karanevildeath12345@gmail.com>
2023-07-23 13:05:24 +0000

    overall code

--- dotnetapp/Webapi/Controllers/SignupController.cs
+++ dotnetapp/Webapi/Controllers/SignupController.cs
@@ -12,22 +12,8 @@ namespace Webapi.Controllers
         {
             BusinessLayer dal = new BusinessLayer();
             return Created("",dal.SaveUser(user));
-        }
-<<<<<<< HEAD
-    }
-}
-=======
-<<<<<<< HEAD
-    }
-}
-=======
-<<<<<<< HEAD
-    }
-}
 
-=======
-<<<<<<< HEAD
-=======
+        }
 
         BusinessLayer dal = new BusinessLayer();
         [HttpGet]
@@ -65,10 +51,6 @@ namespace Webapi.Controllers
             return(dal.dashboard());
         }
 
-
->>>>>>> 68c33ef6a8d67ca4dd708c1bbe958082c80ab738
     }
 }
->>>>>>> 42a1df4aa7e60bdeac85759d215b4ca7fd16040d
->>>>>>> 7159bf44cc39b8d8af92fc9321edd24026ad0a45
->>>>>>> 46f7519582fbe2fdbe5b902d3bb9d8dfef0b9f34
+
