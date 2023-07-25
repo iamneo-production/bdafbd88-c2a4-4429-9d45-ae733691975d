9cda9b81688dedb0a50c215a568961555329af59
pavan1806 <pa1chintakayala@gmail.com>
2023-07-25 08:14:18 +0000

    Backup Commit

---
+++ dotnetapp/Webapi/BusinessLayer.cs
@@ -0,0 +1,14 @@
+namespace Webapi
+{
+    internal class BusinessLayer
+    {
+        DataAccessLayer dal = new DataAccessLayer();
+        public string SaveUser(UserModel user)
+        {
+            return (dal.SaveUser(user));
+        }
+         
+        
+    }
+
+}
